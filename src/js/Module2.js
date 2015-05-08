ccb9306b66cf02552a8a48b0713ffcc5a6bc55ee
kk <kundan100@users.noreply.github.com>
2015-05-08 18:15:00 +0530

    Corrects as per jshint

--- src/js/Module2.js
+++ src/js/Module2.js
@@ -7,7 +7,7 @@ define([], function() {
 		this.init = function() {
 			console.log("Module2 > init...");
 			addEvents();
-		}
+		};
 		
 		this.update = function() {
 			console.log("Module2 > update...");
@@ -15,11 +15,11 @@ define([], function() {
 			//$.Topic("module2Updated").publish("module2 > update > done...");
 			//$.Deferred().done( $.Topic("module2Updated").publish).resolve("module2 > update > done...");
 			appGlobals.globals.publish("module2Updated", {"name":"module2 > update > done..."});
-		}
+		};
 						
 		var addEvents = function(){
 			console.log("Module2 > addEvents...");
-		}
-	}	
+		};
+	};	
 	return Module2;
 });
