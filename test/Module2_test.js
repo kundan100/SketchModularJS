10d7142f2f2e3282cc8a103bf9c6f9f0da36fe70
kundan100 <kundan100@yahoo.com>
2015-05-24 06:22:34 +0530

    1. Adds new test file to run on browser 2. Updates test JS file for node/browser compatibility

--- test/Module2_test.js
+++ test/Module2_test.js
@@ -1,29 +1,48 @@
-var requirejs = require("requirejs");
-var assert = require("assert");
-requirejs.config({
-	baseUrl: 'js',
-    nodeRequire: require
-});
+//console.log(typeof requirejs)
+if( typeof requirejs === 'undefined' ) {
+	//for NodeJS
+	var requirejs = require("requirejs"); //needed to load require/AMD modules
+	var chai = require("chai"); //var assert = require("assert");
+	var expect = chai.expect;
+	requirejs.config({
+		baseUrl: 'js',
+		nodeRequire: require
+	});	
+}else{
+	//for Browser
+	//var assert = require("../node_modules/chai/chai.js"); //var assert = require("assert");
+	var expect = chai.expect;
+}
+/*console.log("requirejs=", requirejs)
+console.log("chai=", chai)
+console.log("expect=", expect)*/
 
-describe('Module2_test _____', function(){
+describe('Module2_test_____', function(){
+	//
 	var module2;
 	before(function(done) {
         requirejs(['../src/js/Module2'], function(Module2) {
-            module2 = new Module2();
+            module2 = new Module2();			
             done(); // We can launch the tests!
         });
     });
 	
-	describe('.add() __', function(){
+	describe('All generic tests__', function(){
+		it('should exist', function(){
+			expect(module2).exist;
+		});
+	});
+	describe('.add()__', function(){
+		
 		it('should return 4 when the parameter is 2 & 2', function(){
-			assert.equal(module2.add(2,2), 4)
+			//assert.equal(module2.add(2,2), 4);
+			expect(module2.add(2,2)).equal(4);
+			
 		});
 		it('should return 0 when any of the parameter is -ve', function(){
-			assert.equal(module2.add(2,-4), 0);
-			assert.equal(module2.add(-5,4), 0);
-			assert.equal(module2.add(-5,-4), 0);
+			//assert.equal(module2.add(2,-4), 0);
+			//assert.equal(module2.add(-5,4), 0);
+			//assert.equal(module2.add(-5,-4), 0);
 		});
-	})
-	
-	
+	});
 });
